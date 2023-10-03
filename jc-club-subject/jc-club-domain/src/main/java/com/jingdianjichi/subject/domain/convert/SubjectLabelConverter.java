31132afd6dba3477434484700e0efe70272a790d
经典鸡翅 <zhq_java@163.com>
2023-10-03 23:21:48 +0800

    feat：标签模块开发

---
+++ jc-club-subject/jc-club-domain/src/main/java/com/jingdianjichi/subject/domain/convert/SubjectLabelConverter.java
@@ -0,0 +1,16 @@
+package com.jingdianjichi.subject.domain.convert;
+
+import com.jingdianjichi.subject.domain.entity.SubjectLabelBO;
+import com.jingdianjichi.subject.infra.basic.entity.SubjectLabel;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+
+@Mapper
+public interface SubjectLabelConverter {
+
+    SubjectLabelConverter INSTANCE = Mappers.getMapper(SubjectLabelConverter.class);
+
+    SubjectLabel convertBoToLabel(SubjectLabelBO subjectLabelBO);
+
+}
--- jc-club-subject/jc-club-domain/src/main/java/com/jingdianjichi/subject/domain/service/impl/SubjectCategoryDomainServiceImpl.java
+++ jc-club-subject/jc-club-domain/src/main/java/com/jingdianjichi/subject/domain/service/impl/SubjectCategoryDomainServiceImpl.java
@@ -20,6 +20,7 @@ public class SubjectCategoryDomainServiceImpl implements SubjectCategoryDomainSe
     @Resource
     private SubjectCategoryService subjectCategoryService;
 
+    @Override
     public void add(SubjectCategoryBO subjectCategoryBO) {
         if (log.isInfoEnabled()) {
             log.info("SubjectCategoryController.add.bo:{}", JSON.toJSONString(subjectCategoryBO));
@@ -30,6 +31,7 @@ public class SubjectCategoryDomainServiceImpl implements SubjectCategoryDomainSe
         subjectCategoryService.insert(subjectCategory);
     }
 
+    @Override
     public List<SubjectCategoryBO> queryCategory(SubjectCategoryBO subjectCategoryBO) {
         SubjectCategory subjectCategory = SubjectCategoryConverter.INSTANCE
                 .convertBoToCategory(subjectCategoryBO);
@@ -44,6 +46,7 @@ public class SubjectCategoryDomainServiceImpl implements SubjectCategoryDomainSe
         return boList;
     }
 
+    @Override
     public Boolean update(SubjectCategoryBO subjectCategoryBO) {
         SubjectCategory subjectCategory = SubjectCategoryConverter.INSTANCE
                 .convertBoToCategory(subjectCategoryBO);
@@ -51,6 +54,7 @@ public class SubjectCategoryDomainServiceImpl implements SubjectCategoryDomainSe
         return count > 0;
     }
 
+    @Override
     public Boolean delete(SubjectCategoryBO subjectCategoryBO) {
         SubjectCategory subjectCategory = SubjectCategoryConverter.INSTANCE
                 .convertBoToCategory(subjectCategoryBO);
