0559cb860ee97e889655a57beb6a2a83567685d2
经典鸡翅 <zhq_java@163.com>
2023-10-14 21:12:06 +0800

    feat：适配器实现oss

--- jc-club-oss/src/main/java/com/jingdianjichi/oss/controller/FileController.java
+++ jc-club-oss/src/main/java/com/jingdianjichi/oss/controller/FileController.java
@@ -1,21 +1,27 @@
 package com.jingdianjichi.oss.controller;
 
-import com.jingdianjichi.oss.util.MinioUtil;
+import com.jingdianjichi.oss.service.FileService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
+/**
+ * 文件操作controller
+ * 
+ * @author: ChickenWing
+ * @date: 2023/10/14
+ */
 @RestController
 public class FileController {
 
     @Resource
-    private MinioUtil minioUtil;
+    private FileService fileService;
 
     @RequestMapping("/testGetAllBuckets")
     public String testGetAllBuckets() throws Exception {
-        List<String> allBucket = minioUtil.getAllBucket();
+        List<String> allBucket = fileService.getAllBucket();
         return allBucket.get(0);
     }
 
---
+++ jc-club-oss/src/main/java/com/jingdianjichi/oss/service/FileService.java
@@ -0,0 +1,31 @@
+package com.jingdianjichi.oss.service;
+
+import com.jingdianjichi.oss.adapter.StorageAdapter;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 文件存储service
+ *
+ * @author: ChickenWing
+ * @date: 2023/10/14
+ */
+@Service
+public class FileService {
+
+    private final StorageAdapter storageAdapter;
+
+    public FileService(StorageAdapter storageAdapter) {
+        this.storageAdapter = storageAdapter;
+    }
+
+    /**
+     * 列出所有桶
+     */
+    public List<String> getAllBucket() {
+        return storageAdapter.getAllBucket();
+    }
+
+}
+
