cf77d57a6a5deb693496bce6e5d0f9a915f74275
经典鸡翅 <zhq_java@163.com>
2023-10-28 23:30:56 +0800

    feat：gateway实现缓存读取权限

--- jc-club-gateway/src/main/java/com/jingdianjichi/club/gateway/auth/StpInterfaceImpl.java
+++ jc-club-gateway/src/main/java/com/jingdianjichi/club/gateway/auth/StpInterfaceImpl.java
@@ -1,8 +1,14 @@
 package com.jingdianjichi.club.gateway.auth;
 
 import cn.dev33.satoken.stp.StpInterface;
+import com.alibaba.cloud.commons.lang.StringUtils;
+import com.google.gson.Gson;
+import com.jingdianjichi.club.gateway.redis.RedisUtil;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -15,22 +21,32 @@ import java.util.List;
 @Component
 public class StpInterfaceImpl implements StpInterface {
 
+    @Resource
+    private RedisUtil redisUtil;
+
+    private String authPermissionPrefix = "auth.permission";
+
+    private String authRolePrefix = "auth.role";
+
     @Override
     public List<String> getPermissionList(Object loginId, String loginType) {
-        // 返回此 loginId 拥有的权限列表
-        // 1、直接跟数据库交互
-        // 2、redis缓存
-        // 3、redis缓存，没有的话，去调用我们的微服务去获取
-        List<String> permissionList = new LinkedList<>();
-        permissionList.add("subject:add");
-        return permissionList;
+        System.out.println("123");
+        return getAuth(loginId.toString(), authPermissionPrefix);
     }
 
     @Override
     public List<String> getRoleList(Object loginId, String loginType) {
-        List<String> roleList = new LinkedList<>();
-        roleList.add("admin");
-        return roleList;
+        return getAuth(loginId.toString(), authRolePrefix);
+    }
+
+    private List<String> getAuth(String loginId, String prefix) {
+        String authKey = redisUtil.buildKey(prefix, loginId.toString());
+        String authValue = redisUtil.get(authKey);
+        if (StringUtils.isBlank(authValue)) {
+            return Collections.emptyList();
+        }
+        List<String> authList = new Gson().fromJson(authValue, List.class);
+        return authList;
     }
 
 }
