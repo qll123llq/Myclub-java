efb4f36f54125293997f836570ccc599b099b5bb
经典鸡翅 <zhq_java@163.com>
2023-10-28 22:54:10 +0800

    feat：gateway实现全局异常处理

--- jc-club-gateway/src/main/java/com/jingdianjichi/club/gateway/auth/SaTokenConfigure.java
+++ jc-club-gateway/src/main/java/com/jingdianjichi/club/gateway/auth/SaTokenConfigure.java
@@ -4,7 +4,6 @@ import cn.dev33.satoken.context.SaHolder;
 import cn.dev33.satoken.reactor.filter.SaReactorFilter;
 import cn.dev33.satoken.router.SaRouter;
 import cn.dev33.satoken.stp.StpUtil;
-import cn.dev33.satoken.util.SaResult;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,15 +24,11 @@ public class SaTokenConfigure {
                 // 鉴权方法：每次访问进入
                 .setAuth(obj -> {
                     System.out.println("-------- 前端访问path：" + SaHolder.getRequest().getRequestPath());
-//                  // 登录校验 -- 拦截所有路由，并排除/user/doLogin 用于开放登录
+                    // 登录校验 -- 拦截所有路由，并排除/user/doLogin 用于开放登录
                     SaRouter.match("/auth/**", "/auth/user/doLogin", r -> StpUtil.checkRole("admin"));
                     SaRouter.match("/oss/**", r -> StpUtil.checkLogin());
                     SaRouter.match("/subject/subject/add", r -> StpUtil.checkPermission("subject:add"));
                     SaRouter.match("/subject/**", r -> StpUtil.checkLogin());
-                })
-                // 异常处理方法：每次setAuth函数出现异常时进入
-                .setError(e -> {
-                    return SaResult.error(e.getMessage());
                 })
                 ;
     }
