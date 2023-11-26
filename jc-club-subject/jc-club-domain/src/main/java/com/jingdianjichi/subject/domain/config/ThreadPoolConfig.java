4668bf8798f5fe0156c31b7a3b51aab339b98946
经典鸡翅 <zhq_java@163.com>
2023-11-26 22:50:20 +0800

    feat:自定义线程工厂

--- jc-club-subject/jc-club-domain/src/main/java/com/jingdianjichi/subject/domain/config/ThreadPoolConfig.java
+++ jc-club-subject/jc-club-domain/src/main/java/com/jingdianjichi/subject/domain/config/ThreadPoolConfig.java
@@ -20,7 +20,8 @@ public class ThreadPoolConfig {
     @Bean(name = "labelThreadPool")
     public ThreadPoolExecutor getLabelThreadPool() {
         return new ThreadPoolExecutor(20, 100, 5,
-                TimeUnit.SECONDS, new LinkedBlockingDeque<>(40), Executors.defaultThreadFactory(),
+                TimeUnit.SECONDS, new LinkedBlockingDeque<>(40),
+                new CustomNameThreadFactory("label"),
                 new ThreadPoolExecutor.CallerRunsPolicy());
     }
 
