bf120c168e94af265640706bd8924a7db0d10b64
经典鸡翅 <zhq_java@163.com>
2024-03-23 12:25:49 +0800

    feat：提交题目

--- jc-club-practice/jc-club-practice-server/src/main/java/com/jingdianjichi/practice/server/service/PracticeDetailService.java
+++ jc-club-practice/jc-club-practice-server/src/main/java/com/jingdianjichi/practice/server/service/PracticeDetailService.java
@@ -1,8 +1,14 @@
 package com.jingdianjichi.practice.server.service;
 
 import com.jingdianjichi.practice.api.req.SubmitPracticeDetailReq;
+import com.jingdianjichi.practice.api.req.SubmitSubjectDetailReq;
 
 public interface PracticeDetailService {
+
+    /**
+     * 练习提交题目
+     */
+    Boolean submitSubject(SubmitSubjectDetailReq req);
 
     /**
      * 提交练题情况
