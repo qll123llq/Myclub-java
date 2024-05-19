fefce3fbbec3815387d07afa8379d4758698b5c0
经典鸡翅 <zhq_java@163.com>
2024-05-19 11:52:58 +0800

    feat:发布内容相关开发

--- jc-club-circle/jc-club-circle-server/src/main/java/com/jingdianjichi/circle/server/config/mybatis/MybatisConfiguration.java
+++ jc-club-circle/jc-club-circle-server/src/main/java/com/jingdianjichi/circle/server/config/mybatis/MybatisConfiguration.java
@@ -1,6 +1,7 @@
 package com.jingdianjichi.circle.server.config.mybatis;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,9 +9,19 @@ import org.springframework.context.annotation.Configuration;
 public class MybatisConfiguration {
 
     @Bean
-    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
         mybatisPlusInterceptor.addInnerInterceptor(new MybatisPlusAllSqlLog());
+        return mybatisPlusInterceptor;
+    }
+
+    @Bean
+    public MybatisPlusInterceptor paginationInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        paginationInnerInterceptor.setOverflow(false);
+        paginationInnerInterceptor.setMaxLimit(500L);
+        mybatisPlusInterceptor.addInnerInterceptor(paginationInnerInterceptor);
         return mybatisPlusInterceptor;
     }
 
