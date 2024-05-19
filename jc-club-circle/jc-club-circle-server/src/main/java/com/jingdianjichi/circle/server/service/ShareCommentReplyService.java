a38dda1ff743791391f4157d40e6c6c89f052c3e
经典鸡翅 <zhq_java@163.com>
2024-05-19 13:28:46 +0800

    feat:评论回复

---
+++ jc-club-circle/jc-club-circle-server/src/main/java/com/jingdianjichi/circle/server/service/ShareCommentReplyService.java
@@ -0,0 +1,22 @@
+package com.jingdianjichi.circle.server.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.jingdianjichi.circle.api.req.RemoveShareCommentReq;
+import com.jingdianjichi.circle.api.req.SaveShareCommentReplyReq;
+import com.jingdianjichi.circle.server.entity.po.ShareCommentReply;
+
+/**
+ * <p>
+ * 评论及回复信息 服务类
+ * </p>
+ *
+ * @author ChickenWing
+ * @since 2024/05/16
+ */
+public interface ShareCommentReplyService extends IService<ShareCommentReply> {
+
+    Boolean saveComment(SaveShareCommentReplyReq req);
+
+    Boolean removeComment(RemoveShareCommentReq req);
+
+}
